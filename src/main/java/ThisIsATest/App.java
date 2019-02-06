9497fa6c8b965b709705d426ceb8476f5775b9e2
turgutreis <a_mazlum@web.de>
2019-02-06 13:55:38 +0100

    Initial commit

---
+++ src/main/java/ThisIsATest/App.java
@@ -0,0 +1,13 @@
+package ThisIsATest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
